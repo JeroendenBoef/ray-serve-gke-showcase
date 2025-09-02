6bb662888a6795421f54d7f621b1890276c12e2e
JeroendenBoef <jeroendenboef1998@gmail.com>
2025-09-02 10:42:23 +0200

    Switch to surfacing module outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,3 @@
-output "cluster_name" { value = var.cluster_name }
-output "region"       { value = var.region }
+output "cluster_name" { value = module.gke.cluster_name }
+output "region"       { value = module.gke.location }
 output "project_id"   { value = var.project_id }
