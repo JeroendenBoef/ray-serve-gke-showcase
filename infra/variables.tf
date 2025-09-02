b3fb2ead9adc887cd8900b7a54db5944ffdb3b6b
JeroendenBoef <jeroendenboef1998@gmail.com>
2025-09-02 21:03:59 +0200

    Removed defaults to parse from .env

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,16 +1,13 @@
 variable "project_id" {
   type    = string
-  default = "ray-serve-gke-showcase-1122"
 }
 
 variable "region" {
   type    = string
-  default = "europe-west4"
 }
 
 variable "zone" {
   type    = string
-  default = "europe-west4-a"
 }
 
 variable "cluster_name" {
