c3819045af54b98db29d0ee110f1325da9351f91
JeroendenBoef <jeroendenboef1998@gmail.com>
2025-09-02 10:41:53 +0200

    Removed duplicate providers block

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
-    }
-  }
-}
-
 module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google"
   version    = "~> 33.0"
