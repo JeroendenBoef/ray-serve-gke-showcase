3c1ce59917f0b3b5d70a71987669cbea397455a4
JeroendenBoef <jeroendenboef1998@gmail.com>
2025-09-06 10:02:21 +0200

    quick sanity check

--- apps/ray/serve_app/test_app.py
+++ apps/ray/serve_app/test_app.py
@@ -1,11 +1,10 @@
 from ray import serve
-from starlette.requests import Request
 
 
 @serve.deployment
 class Echo:
-    async def __call__(self, request: Request):
-        return {"echo": (await request.json())}
+    async def __call__(self, request):
+        return {"echo": await request.json()}
 
 
 app = serve.deployment_graph.bind(Echo.bind())
